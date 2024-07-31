a5373e2024ddfd4c6a5d6218b3bc27cdf616e402
lucas <lucassilva.work@outlook.com>
2024-07-30 22:58:35 -0300

    update origin allowed

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,7 +1,18 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '86400');
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3002',
+  ]
+
+  const origin = request.header('Origin');
+  const isAllowed = allowedOrigins.includes(origin);
+
+  if (isAllowed) {
+    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '86400');
+  }
+
   next();
 };
